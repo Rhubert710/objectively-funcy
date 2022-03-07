f2d36237b86d2cfb08781c850488a7a097ed2997
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-03-07 09:21:23 -0500

    added hw

--- main.js
+++ main.js
@@ -2,6 +2,46 @@
 
 
 
+function getFirstName (person) {
+  return person.firstName;
+}
+
+function getLastName (person) {
+  return person.lastName;
+}
+
+function getFullName (person) {
+  return person.firstName + " " + person.lastName;
+}
+
+function setFirstName (person, person2firstName) {
+  return person.firstName = person2firstName;
+}
+
+function setAge (person, person2age) {
+  return person.age = person2age;
+}
+
+function giveBirthday (person) {
+  if (person.age === undefined) {
+    person.age = 0;
+  }
+  person.age++
+}
+
+function marry (person, person2married) {
+    person.married = true;
+    person2married.married = true;
+    person.spouseName = person2married.firstName + " " + person2married.lastName;
+    person2married.spouseName = person.firstName + " " + person.lastName;
+}
+
+function divorce (person, person2divorced) {
+  person.married = false;
+  person2divorced.married = false;
+  delete person.spouseName;
+  delete person2divorced.spouseName;
+}
 
 
 // Our code here. Don't touch!
